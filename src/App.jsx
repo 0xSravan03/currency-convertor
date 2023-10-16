67a14d68285da48a8285433e2854b708e477e925
Sravan T P <sravantp03@gmail.com>
2023-10-16 11:35:38 +0530

    added convert + swap options

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,8 @@ export default function App() {
   const [convertedAmount, setConvertedAmount] = useState(0);
   const currencyData = useCurrency(from); // using custom hook for fetching currency price
 
-
   function convert() {
-    setConvertedAmount(amount * currencyData[to]);
+    setConvertedAmount((amount * currencyData[to]).toFixed(2));
   }
 
   function swap() {
@@ -22,14 +21,26 @@ export default function App() {
   }
 
   return (
-    <main className="w-screen h-screen bg-slate-400 flex items-center justify-center px-2">
-      <div className="w-full relative max-w-[500px] flex flex-col justify-center gap-4 p-6 bg-slate-500 rounded-lg">
+    <main className="w-screen h-screen flex items-center justify-center px-2">
+      <div className="w-full relative max-w-[550px] flex flex-col justify-center gap-4 p-8 bg-cyan-300 rounded-lg">
         <button id="swap" onClick={swap} className="absolute left-[45%] top-[34%] bg-blue-500 p-2 rounded-md text-white">
           <img src="https://cdn-icons-png.flaticon.com/128/7166/7166841.png" alt="swap" width={28} />
         </button>      
-        <InputBox />
-        <InputBox />
-        <button className="p-4 rounded-lg bg-blue-600 text-white">Convert</button>
+        <InputBox
+          label="From"
+          amount={amount}
+          onAmountChange={setAmount}
+          onCurrencyChange={setFrom}
+          selectedCurrency={from}
+        />
+        <InputBox
+          label="To"
+          amount={convertedAmount}
+          onCurrencyChange={setTo}
+          selectedCurrency={to}
+          amountDisabled={true}
+        />
+        <button className="p-4 rounded-lg bg-blue-600 text-white" onClick={convert}>Convert</button>
       </div>
     </main>
   );
